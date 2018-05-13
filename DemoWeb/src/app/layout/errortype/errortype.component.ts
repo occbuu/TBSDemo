2c53ccaaef95932a8c0cd1902d215580935da7bd
Nlduong <nlduong2007@gmail.com>
2018-05-13 15:36:16 +0700

    Design Error page

--- DemoWeb/src/app/layout/errortype/errortype.component.ts
+++ DemoWeb/src/app/layout/errortype/errortype.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { routerTransition } from '../../router.animations';
+import { ModalDirective, TooltipModule } from 'ngx-bootstrap';
 
 @Component({
     selector: 'app-errortype',
@@ -9,6 +10,7 @@ import { routerTransition } from '../../router.animations';
 })
 export class ErrortypeComponent implements OnInit {
     constructor() {}
+    @ViewChild('popMsg') public popMsg: ModalDirective;
 
     ngOnInit() {}
 }
--- DemoWeb/src/app/layout/errortype/errortype.module.ts
+++ DemoWeb/src/app/layout/errortype/errortype.module.ts
@@ -4,9 +4,12 @@ import { CommonModule } from '@angular/common';
 import { ErrortypeRoutingModule } from './errortype-routing.module';
 import { ErrortypeComponent } from './errortype.component';
 import { PageHeaderModule } from './../../shared';
+import { ModalModule } from 'ngx-bootstrap/modal';
+import { FormsModule } from '@angular/forms';
+import { CollapseModule, TooltipModule } from 'ngx-bootstrap';
 
 @NgModule({
-    imports: [CommonModule, ErrortypeRoutingModule, PageHeaderModule],
+    imports: [CommonModule, ErrortypeRoutingModule, PageHeaderModule,ModalModule.forRoot(),],
     declarations: [ErrortypeComponent]
 })
 export class ErrortypeModule {}
