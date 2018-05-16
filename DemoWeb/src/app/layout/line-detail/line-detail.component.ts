0884cc07a82ac4d59bd09f2a9d1fe71811e12931
Hao Lee <occbuu>
2018-05-16 20:40:15 +0700

    Update Line detail...

--- DemoWeb/src/app/layout/line-detail/line-detail.component.ts
+++ DemoWeb/src/app/layout/line-detail/line-detail.component.ts
@@ -4,11 +4,41 @@ import { routerTransition } from '../../router.animations';
 @Component({
     selector: 'app-line-detail',
     templateUrl: './line-detail.component.html',
-    styleUrls: ['./line-detail.component.scss'],
+    styleUrls: [
+        './line-detail.component.scss',
+    ],
     animations: [routerTransition()]
 })
 export class LineDetailComponent implements OnInit {
+    modelfd: any = new Date(2018, 5, 4);
+    modeltd: any = new Date(2018, 5, 20);
+    isSearched = false;
+   
+    // Pie
+     public pieChartLabels: string[] = [
+        'Good Qty',
+        'Major Error',
+        'Minor Errors',
+        'Bad Errors'
+    ];
+    public pieChartData: number[] = [300, 500, 100, 100];
+    public pieChartType: string = 'pie';
+
+    public pieChartData1: number[] = [500, 50, 10, 10];
+    public pieChartType1: string = 'pie';
+
+    public pieChartData2: number[] = [800, 500, 100, 100];
+    public pieChartType2: string = 'pie';
+
     constructor() {}
 
     ngOnInit() {}
+
+    search()
+    {
+        if(this.isSearched)
+            this.isSearched = false;
+            else
+            this.isSearched = true;
+    }
 }
--- DemoWeb/src/app/layout/line-detail/line-detail.module.ts
+++ DemoWeb/src/app/layout/line-detail/line-detail.module.ts
@@ -1,12 +1,22 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ChartsModule as Ng2Charts } from 'ng2-charts';
 
 import { LineDetailRoutingModule } from './line-detail-routing.module';
 import { LineDetailComponent } from './line-detail.component';
 import { PageHeaderModule } from './../../shared';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { FormsModule } from '@angular/forms';
+
 
 @NgModule({
-    imports: [CommonModule, LineDetailRoutingModule, PageHeaderModule],
+    imports: [
+        CommonModule, 
+        FormsModule,
+        LineDetailRoutingModule, 
+        NgbModule.forRoot(), 
+        Ng2Charts,       
+        PageHeaderModule],
     declarations: [LineDetailComponent]
 })
 export class LineDetailModule {}
