c90513d14d539e8d8aceb8577b954a4496f76023
Nlduong <nlduong2007@gmail.com>
2018-05-16 13:47:37 +0700

    add Rule search

--- DemoWeb/src/app/layout/rule/rule.module.ts
+++ DemoWeb/src/app/layout/rule/rule.module.ts
@@ -4,9 +4,11 @@ import { CommonModule } from '@angular/common';
 import { RuleRoutingModule } from './rule-routing.module';
 import { RuleComponent } from './rule.component';
 import { PageHeaderModule } from './../../shared';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { TreeviewModule } from 'ngx-treeview';
 
 @NgModule({
-    imports: [CommonModule, RuleRoutingModule, PageHeaderModule],
+    imports: [CommonModule, RuleRoutingModule, PageHeaderModule,NgbModule.forRoot(),],
     declarations: [RuleComponent]
 })
 export class RuleModule {}
