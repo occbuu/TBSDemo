9a686369c5e3662c7ba907145203e823158f60c3
Hao Lee <occbuu>
2018-05-16 15:11:18 +0700

    Update [Add new Line] UI...

--- DemoWeb/src/app/layout/line-new/line-new.component.ts
+++ DemoWeb/src/app/layout/line-new/line-new.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { routerTransition } from '../../router.animations';
 
+
 @Component({
     selector: 'app-line-new',
     templateUrl: './line-new.component.html',
@@ -8,6 +9,9 @@ import { routerTransition } from '../../router.animations';
     animations: [routerTransition()]
 })
 export class LineNewComponent implements OnInit {
+    public isCollapsed = false;
+    StartingDate: any;
+    DeadlineDate: any;
     constructor() {}
 
     ngOnInit() {}
--- DemoWeb/src/app/layout/line-new/line-new.module.ts
+++ DemoWeb/src/app/layout/line-new/line-new.module.ts
@@ -1,12 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
+import { FormsModule } from '@angular/forms';
 import { LineNewRoutingModule } from './line-new-routing.module';
 import { LineNewComponent } from './line-new.component';
 import { PageHeaderModule } from './../../shared';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
-    imports: [CommonModule, LineNewRoutingModule, PageHeaderModule],
+    imports: [
+        CommonModule, 
+        LineNewRoutingModule,         
+        NgbModule.forRoot(),
+        FormsModule,
+        PageHeaderModule],
     declarations: [LineNewComponent]
 })
 export class LineNewModule {}
